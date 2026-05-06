56b214551ec9820b8fe3bb7f1b2e5eb13253ea5f
Kryptonite Ali <cryptonite.ali@gmail.com>
2026-05-06 18:19:05 +0800

    Improve elegant interactive landing page

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ export default function App() {
             <a href="#uses">Use Cases</a>
             <a href="#wallet">Wallet</a>
             <a href="#compliance">Compliance</a>
-            <a href="https://tradeport.xyz" target="_blank" rel="noreferrer" className="navBtn">
+            <a href="https://www.tradeport.xyz/iota/collection/0x6d2c743607ef275bd6934fe5c2a7e5179cca6fbd2049cfa79de2310b74f3cf83%3A%3Aname_registration%3A%3ANameRegistration?bottomTab=trades&tab=items&tokenId=0x0e444e3a3f543fb7ce87b0ae0c913186e132962997852254026521e0795399af&modalSlug=0x6d2c743607ef275bd6934fe5c2a7e5179cca6fbd2049cfa79de2310b74f3cf83%3A%3Aname_registration%3A%3ANameRegistration&nav=1" target="_blank" rel="noreferrer" className="navBtn">
               Make an Offer
             </a>
           </div>
@@ -60,7 +60,7 @@ export default function App() {
             </p>
 
             <div className="actions">
-              <a href="https://tradeport.xyz" target="_blank" rel="noreferrer" className="primary">
+              <a href="https://www.tradeport.xyz/iota/collection/0x6d2c743607ef275bd6934fe5c2a7e5179cca6fbd2049cfa79de2310b74f3cf83%3A%3Aname_registration%3A%3ANameRegistration?bottomTab=trades&tab=items&tokenId=0x0e444e3a3f543fb7ce87b0ae0c913186e132962997852254026521e0795399af&modalSlug=0x6d2c743607ef275bd6934fe5c2a7e5179cca6fbd2049cfa79de2310b74f3cf83%3A%3Aname_registration%3A%3ANameRegistration&nav=1" target="_blank" rel="noreferrer" className="primary">
                 Buy it on TradePort →
               </a>
 
